2d43f6022e315c406d1fdaa82f441b870467c5ba
Justin Auger <justnaugr@gmail.com>
2024-05-01 15:27:24 -0400

    better mobile layout

--- app/nata.tsx
+++ app/nata.tsx
@@ -2,29 +2,29 @@ import { useState } from "react";
 import Image from "next/image";
 
 export default function Nata() {
-    const [nataImage, setNataImage] = useState(14);
+  const [nataImage, setNataImage] = useState(14);
 
-    const images = [];
-    for (let i = 1; i <= 15; i++ ) {
-      images.push(<Image
-        className="rounded-3xl max-md:max-w-50 max-md:max-h-50 w-150 h-150 hover:cursor-pointer"
-        src={`/cat/${i}.jpg`}
-        alt="Photo of Justin (that's me, the narrator), looking fantastic"
-        width="250"
-        height="250"
-        onClick={() => {
-          let random = 1 + Math.floor(Math.random() * 13);
-          //basic prevention of repetition
-          if (nataImage == random)
-            random++;
-          setNataImage(random);
-        }}
-      />);
-    }
+  const images = [];
+  for (let i = 1; i <= 15; i++) {
+    images.push(<Image
+      className="rounded-3xl max-md:max-w-50 max-md:max-h-50 w-150 h-150 hover:cursor-pointer"
+      src={`/cat/${i}.jpg`}
+      alt="Photo of Justin (that's me, the narrator), looking fantastic"
+      width="250"
+      height="250"
+      onClick={() => {
+        let random = 1 + Math.floor(Math.random() * 13);
+        //basic prevention of repetition
+        if (nataImage == random)
+          random++;
+        setNataImage(random);
+      }}
+    />);
+  }
 
-    return (
-        <div className="order-2 z-10 flex peer justify-center pt-2 pb-5 ">
-            {images[nataImage]}
-        </div>
-    );
+  return (
+    <div className="order-2 z-10 flex peer justify-center pt-2 pb-5 ">
+      {images[nataImage]}
+    </div>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -22,8 +22,8 @@ export default function Home() {
   return (
     <div className={grotesk.className}>
       <main className="bg-slate-50 px-10 bg-gradient-to-b to-white from-teal-50 dark:to-stone-800 dark:from-stone-900">
-        <section className="min-h-svh flex flex-col">
-          <div className="pt-10 mb-12 flex justify-between">
+        <section className="flex min-h-svh flex-col">
+          <div className="pt-5 md:pt-10 mb-5 md:mb-12 flex justify-between">
             <div className="flex">
               <h1>justin</h1>
               <h1 className="text-teal-800 dark:text-emerald-400">@</h1>
@@ -39,56 +39,58 @@ export default function Home() {
                   (<MoonIcon className="w-8 h-8 cursor-pointer" onClick={() => setTheme('dark')} />)
             }
           </div>
-          <div className="flex flex-col justify-center text-center pt-10 max-md:pt-0">
-            <Nata />
-            <div className="order-1 relative z-0 top-10 transition ease-in-out duration-500 peer-has-[:hover]:-translate-y-10">
-              click on Nata for a surprise!
+          <div className="flex flex-col justify-between flex-1">
+            <div className="flex flex-col justify-center text-center max-md:pt-0">
+              <Nata />
+              <div className="order-1 relative z-0 top-10 transition ease-in-out duration-500 peer-has-[:hover]:-translate-y-10">
+                click on Nata for a surprise!
+              </div>
+              <h2 className={`order-3 text-3xl md:text-5xl lg:text-5xl md:py-2 pb-2 md:pb-4 text-teal-800 ${groteskBold.className} dark:text-emerald-400`}>Justin Auger</h2>
+              <Title />
+              <div className="order-4 flex justify-center gap-10 pt-2 md:pt-5 text-gray-800">
+                <Link href="mailto:justin@auger.dev">
+                  <EnvelopeIcon
+                    className="w-8 h-8 md:w-10 md:h-10 cursor-pointer hover:text-teal-700 dark:text-white dark:hover:text-emerald-400"
+                    aria-valuetext="email me"
+                    onMouseEnter={() => setHoverText('Send me an email')}
+                    onMouseLeave={() => setHoverText('')}
+                  />
+                </Link>
+                <Link href="/resume.pdf">
+                  <DocumentTextIcon
+                    className="w-8 h-8 md:w-10 md:h-10 cursor-pointer hover:text-teal-700 dark:text-white dark:hover:text-emerald-400"
+                    aria-valuetext="resume"
+                    onMouseEnter={() => setHoverText('Check out my resume')}
+                    onMouseLeave={() => setHoverText('')}
+                  />
+                </Link>
+                <Link href="https://www.linkedin.com/in/justinauger/">
+                  <BriefcaseIcon
+                    className="w-8 h-8 md:w-10 md:h-10 cursor-pointer hover:text-teal-700 dark:text-white dark:hover:text-emerald-400"
+                    aria-valuetext="linkedin"
+                    onMouseEnter={() => setHoverText('Find me on LinkedIn')}
+                    onMouseLeave={() => setHoverText('')}
+                  />
+                </Link>
+                <Link href="https://github.com/JustnAugr">
+                  <CommandLineIcon
+                    className="w-8 h-8 md:w-10 md:h-10 cursor-pointer hover:text-teal-700 dark:text-white dark:hover:text-emerald-400"
+                    aria-valuetext="github"
+                    onMouseEnter={() => setHoverText('Critique my GitHub projects')}
+                    onMouseLeave={() => setHoverText('')}
+                  />
+                </Link>
+              </div>
+              {/* sadly not using peer here as I want to set the hover text and peer-has-a[:hover] won't tell me which one I'm hovering over */}
+              {hoverText && <div className='order-5 flex justify-center pt-5 text-teal-800 dark:text-emerald-400'>
+                {hoverText}
+              </div>}
             </div>
-            <h2 className={`order-3 text-3xl md:text-5xl lg:text-5xl py-2 pb-4 text-teal-800 ${groteskBold.className} dark:text-emerald-400`}>Justin Auger</h2>
-            <Title />
-          </div>
-          <div className="flex justify-center gap-10 pt-5 text-gray-800">
-            <Link href="mailto:justin@auger.dev">
-              <EnvelopeIcon
-                className="w-8 h-8 md:w-10 md:h-10 cursor-pointer hover:text-teal-700 dark:text-white dark:hover:text-emerald-400"
-                aria-valuetext="email me"
-                onMouseEnter={() => setHoverText('Send me an email')}
-                onMouseLeave={() => setHoverText('')}
-              />
-            </Link>
-            <Link href="/resume.pdf">
-              <DocumentTextIcon
-                className="w-8 h-8 md:w-10 md:h-10 cursor-pointer hover:text-teal-700 dark:text-white dark:hover:text-emerald-400"
-                aria-valuetext="resume"
-                onMouseEnter={() => setHoverText('Check out my resume')}
-                onMouseLeave={() => setHoverText('')}
-              />
-            </Link>
-            <Link href="https://www.linkedin.com/in/justinauger/">
-              <BriefcaseIcon
-                className="w-8 h-8 md:w-10 md:h-10 cursor-pointer hover:text-teal-700 dark:text-white dark:hover:text-emerald-400"
-                aria-valuetext="linkedin"
-                onMouseEnter={() => setHoverText('Find me on LinkedIn')}
-                onMouseLeave={() => setHoverText('')}
-              />
-            </Link>
-            <Link href="https://github.com/JustnAugr">
-              <CommandLineIcon
-                className="w-8 h-8 md:w-10 md:h-10 cursor-pointer hover:text-teal-700 dark:text-white dark:hover:text-emerald-400"
-                aria-valuetext="github"
-                onMouseEnter={() => setHoverText('Critique my GitHub projects')}
-                onMouseLeave={() => setHoverText('')}
-              />
-            </Link>
-          </div>
-          {/* sadly not using peer here as I want to set the hover text and peer-has-a[:hover] won't tell me which one I'm hovering over */}
-          {hoverText && <div className='flex justify-center pt-5 text-teal-800 dark:text-emerald-400'>
-            {hoverText}
-          </div>}
-          <div className="absolute bottom-5 self-center">
-            <p className="text-sm text-gray-700 dark:text-stone-400">
-              brought to us by next.js
-            </p>
+            <div className="flex justify-center pb-5">
+              <p className="text-sm text-gray-700 dark:text-stone-400">
+                brought to us by next.js
+              </p>
+            </div>
           </div>
         </section>
       </main>
