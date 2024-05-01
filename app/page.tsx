9709e8901bce3f4235e77df950952ab9af7a905f
Justin Auger <justnaugr@gmail.com>
2024-04-30 21:19:02 -0400

    create all the images and store in array

--- app/page.tsx
+++ app/page.tsx
@@ -13,19 +13,26 @@ export default function Home() {
 
   const [hoverText, setHoverText] = useState('');
   const [mounted, setMounted] = useState(false);
-  const [nataImage, setNataImage] = useState('/surprise.jpg');
+  const [nataImage, setNataImage] = useState(14);
 
   useEffect(() => setMounted(true), []);
 
-  function getRandomImage() {
-    const random = 1 + Math.floor(Math.random() * 13);
-    const imageUrl = `/cat/${random}.jpg`;
-    if (imageUrl == nataImage) {
-      //if we randomly got the same int, just progress to the next
-      setNataImage(`/cat/${random + 1}.jpg`);
-    } else {
-      setNataImage(`/cat/${random}.jpg`);
-    }
+  const images = [];
+  for (let i = 1; i <= 15; i++ ) {
+    images.push(<Image
+      className="rounded-3xl max-md:max-w-50 max-md:max-h-50 w-150 h-150"
+      src={`/cat/${i}.jpg`}
+      alt="Photo of Justin (that's me, the narrator), looking fantastic"
+      width="250"
+      height="250"
+      onClick={() => {
+        let random = 1 + Math.floor(Math.random() * 13);
+        //basic prevention of repetition
+        if (nataImage == random)
+          random++;
+        setNataImage(random);
+      }}
+    />);
   }
 
   return (
@@ -48,16 +55,9 @@ export default function Home() {
                   (<MoonIcon className="w-8 h-8 cursor-pointer" onClick={() => setTheme('dark')} />)
             }
           </div>
-          <div className="flex flex-col justify-center text-center pt-20 max-md:pt-0">
-            <div className="order-2 flex peer justify-center pt-2 pb-5 " >
-              <Image
-                className="rounded-3xl max-md:w-50 max-md:h-50 w-150 h-150"
-                src={nataImage}
-                alt="Photo of Justin (that's me, the narrator), looking fantastic"
-                width="250"
-                height="250"
-                onClick={() => getRandomImage()}
-              />
+          <div className="flex flex-col justify-center text-center pt-10 max-md:pt-0">
+            <div className="order-2 flex peer justify-center pt-2 pb-5 ">
+              {images[nataImage]}
             </div>
             <div className="order-1 flex justify-center invisible transition ease-in-out duration-500 peer-has-[:hover]:-translate-y-2 peer-has-[:hover]:visible">
               click on Nata for a surprise!
